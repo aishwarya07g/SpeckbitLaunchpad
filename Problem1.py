3f2dd06cbad1ac56a866ec90eb24d2b528fb8d24
aishwarya07g <42799570+aishwarya07g@users.noreply.github.com>
2020-02-27 01:37:17 +0530

    Add files via upload

---
+++ Problem1.py
@@ -0,0 +1,7 @@
+name = input("Enter Name: ")
+age = int(input("Enter Age: "))
+
+current = 2020
+yearofbirth = 2020 - age 
+
+print("Hey " + name + " !" + "You'll turn 100 years old in "+ str(yearofbirth+100) )
